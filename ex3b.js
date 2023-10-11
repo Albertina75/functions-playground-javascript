defbb0a50244efe4abda6951fd62c5d7b2e45c65
Albertina75 <145276835+Albertina75@users.noreply.github.com>
2023-10-11 12:15:25 +0200

    ex3. js ex3b.js

--- ex3b.js
+++ ex3b.js
@@ -7,10 +7,11 @@ function isPositive(num) {
 }
 
 /**
- * 1. ¿Que nombre tiene esta función?
- * 2. ¿Que deberíamos escribir para ejecutar esta función?
- * 3. ¿Qué tipo de datos devuelve esta función? ¿Un string, un number o un boolean?
- * 4. El código de abajo usa la función isPositve. ¿La usa adecuadamente? ¿Por qué?
+ * 1. ¿Que nombre tiene esta función? Esta función tiene como nombre isPositive.
+ * 2. ¿Que deberíamos escribir para ejecutar esta función? Un número como argumento.
+ * 3. ¿Qué tipo de datos devuelve esta función? ¿Un string, un number o un boolean? Un boolean ya que devuelve un true o false.
+ * 4. El código de abajo usa la función isPositve. ¿La usa adecuadamente? ¿Por qué? No la usa adecuadamente porque invoca 'El número es negativo antes que positivo. 
+      Para que funcionase el mensaje debería cambiar de orden en el console.log.
  */
 
 let x = 4;
